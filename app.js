4ec1a7d7f0ffa054821e310001c961cbfdaa1771
Wesley Diehl <wesleydiehl@live.com>
2023-06-26 19:02:34 -0300

    teste

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const cliche = "Olá Mundo!"
-console.log(cliche)
+const cliche = "Olá Mundo!";
+let multiplicacao;
+let n1, n2;
+
+n1 = 10;
+n2 = 3.33
+
+multiplicacao = n1*n2;
+
+console.log(multiplicacao);
+
+
