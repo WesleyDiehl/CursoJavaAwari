e1617a4cb48c7af1066b2081e315c9b7d41e3237
WesleyDiehl <wesleydiehl@live.com>
2023-08-03 17:35:17 -0300

    aula 02 busca cep

--- 1-primeiro-exemplo/Teste.js
+++ 1-primeiro-exemplo/Teste.js
@@ -1,11 +1,32 @@
-let nome = 'awari';
+/*let nome = 'awari';
 console.log(nome);
 nome = 10;
 console.log(nome);
 
-let rarold, cleyton;
+let xc, yc;
 
-rarold = 13*20;
-cleyton = rarold**2;
-console.log(cleyton, rarold);
+xc = 1*20;
+yc = xc**2;
+console.log(yc, xc);*/
 
+const btnBuscaCep = document.getElementById('btnBuscaCep');
+
+btnBuscaCep.addEventListener('click', () => {
+    const cep = document.getElementById('cep').value;
+    console.log(cep);
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then((res) => {
+        return res.json();
+    }).then((response) => {
+      if (response) {       
+        const endereco = document.getElementById('endereco'); 
+        endereco.innerHTML =`
+        <b>Rua: </b> ${response.logradouro}
+        <b>Bairro: </b> ${response.bairro}
+        <b>Municipio: </b> ${response.localidade}
+        <b>UF: </b> ${response.uf}
+        ` 
+      } 
+    }).catch((error) => {
+        console.error(error);
+    })
+});
