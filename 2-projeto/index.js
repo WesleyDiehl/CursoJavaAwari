297ae7069bad7a81a588aaa275e67f2c7ed89cec
WesleyDiehl <wesleydiehl@live.com>
2023-08-14 19:44:44 -0300

    atualizando a aula

--- 2-projeto/index.js
+++ 2-projeto/index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const axios = require('axios');
 const server = express();
 
-server.get('/awari', (req, res) => {
-    const result = axios.get('https://viacep.com.br/ws/01001000/json/');
-    console.log('result');
-    return res.send('Bom dia teste sera?');
+server.get('/awari', async (req, res) => {
+    const result = await axios.get(`https://viacep.com.br/ws/${req.query.cep}/json`);
+    return res.send(result.data);
 });
 
 server.listen(3333);
