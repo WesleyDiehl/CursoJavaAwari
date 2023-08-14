d4ff3dfe1b47c5669e3152773dc0d71f81aa962e
WesleyDiehl <wesleydiehl@live.com>
2023-08-14 19:33:13 -0300

    teste

--- 2-projeto/index.js
+++ 2-projeto/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const axios = require('axios');
 const server = express();
 
 server.get('/awari', (req, res) => {
-    console.log(req.query);
-    return res.send('Bom dia teste');
+    const result = axios.get('https://viacep.com.br/ws/01001000/json/');
+    console.log('result');
+    return res.send('Bom dia teste sera?');
 });
 
 server.listen(3333);
